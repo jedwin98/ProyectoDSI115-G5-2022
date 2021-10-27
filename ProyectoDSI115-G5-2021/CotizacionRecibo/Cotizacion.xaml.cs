2dace9c739b6afc3250458f71566b32b3f0ce38b
Edwin Jiménez <edwinjimenezp98@gmail.com>
2021-10-26 19:47:15 -0600

    Carga de datos a la tabla de selección y programación de busqueda

--- ProyectoDSI115-G5-2021/CotizacionRecibo/Cotizacion.xaml.cs
+++ ProyectoDSI115-G5-2021/CotizacionRecibo/Cotizacion.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,9 +20,24 @@ namespace ProyectoDSI115_G5_2021.CotizacionRecibo
     /// </summary>
     public partial class Cotizacion : Window
     {
+
+        ControlBD control;
+        DataTable dt = new DataTable();
+
+        DataTable dataDetalle = new DataTable();
+
+
         public Cotizacion()
         {
             InitializeComponent();
+            cargarTabla();
+        }
+
+        public void cargarTabla()
+        {
+            control = new ControlBD();
+            dt = control.consultarInve();
+            dataInventario.ItemsSource = dt.DefaultView;
         }
 
         private void BtnVolver_Click(object sender, RoutedEventArgs e)
@@ -29,5 +45,16 @@ namespace ProyectoDSI115_G5_2021.CotizacionRecibo
             this.Close();
         }
 
+        private void BuscarProMat()
+        {
+            dt.Clear();
+            dt = control.BuscarInve(txtBusqueda.Text);
+            dataInventario.ItemsSource = dt.DefaultView;
+        }
+
+        private void BtnBusqueda_Click(object sender, RoutedEventArgs e)
+        {
+            BuscarProMat();
+        }
     }
 }
